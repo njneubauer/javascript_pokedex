94a02ba777461fdddc8e66d22ac5b3bad3209624
njneubauer <njneubauer@gmail.com>
2021-09-14 21:56:25 -0500

    displayed pokemonList array onto index.html via script.js. Added comment for tallest/shortest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,70 @@
 let pokemonList = [
-    {name: 'Pikachu', Height: 1.3, Type: 'electric'},
-    {name: 'Charizard', Height: 5.5, Type: ['fire', 'flying']},
-    {name: 'Venusaur', Height: 6.5, Type: 'electric'},
-    {name: 'Squirtle', Height: 1.6, Type: ['grass', 'poison']}
+    {
+        name: 'Pikachu', 
+        height: 1.3, 
+        type: 'electric'
+    },
+    {
+        name: 'Charizard',
+        height: 5.5,
+        type: ['fire', 'flying']
+    },
+    {
+        name: 'Venusaur', 
+        height: 6.5,
+        type: 'electric'
+    },
+    {
+        name: 'Squirtle',
+        height: 1.6,
+        type: ['grass', 'poison']
+    }
 ];
 
-console.log(pokemonList)
+let tallestPokemon = 0;
+let shortestPokemon = 0;
+
+for (let i=0; i<pokemonList.length; i++){
+    // Initialize height variables with height of first pokemon in array
+    if(i==0){
+        tallestPokemon = pokemonList[0].height;
+        shortestPokemon = pokemonList[0].height;
+    }
+    // Find tallest pokemon height
+    if(pokemonList[i].height>tallestPokemon){
+        tallestPokemon = pokemonList[i].height;
+    }
+    // Find shortest pokemon height
+    if(pokemonList[i].height<shortestPokemon){
+        shortestPokemon = pokemonList[i].height;
+    }
+};
+
+console.log(tallestPokemon);
+console.log(shortestPokemon);
+
+let tallPokemonComment = "Wow, this pokemon has stilts for legs!";
+let smallPokemonComment = "Small. but MIGHTY!";
+
+for (let i=0; i<pokemonList.length; i++){
+    // add opening <ul> tag
+    if (i<1){
+        document.write('<ul>');
+    };
+
+    // Add items <li> to <ul>. Add comments for shortest and tallest pokemon based on their height.
+    if(pokemonList[i].height==tallestPokemon){
+        document.write('<li>' + pokemonList[i].name +' '+ `(Height:${pokemonList[i].height})`+ `--${tallPokemonComment}` +'</li>');
+    }
+    else if(pokemonList[i].height==shortestPokemon){
+        document.write('<li>' + pokemonList[i].name +' '+ `(Height:${pokemonList[i].height})`+ `--${smallPokemonComment}` +'</li>');
+    }
+    else {
+        document.write('<li>' + pokemonList[i].name +' '+ `(Height:${pokemonList[i].height})`+ '</li>');
+    }
+
+    // add closing </ul> tag
+    if (i==pokemonList.length){
+        document.write('</ul>');
+    }
+};
