ccc6ae8deb2075b4f86cb1dc64980f95b7c2e492
njneubauer <njneubauer@gmail.com>
2021-09-24 20:14:12 -0500

    added funcs: loadList, loadDetails, showLoadingGif, & hideLoadingGif. hit pokemon api to populate buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,6 @@
 let pokemonRepository = (function(){
-    let pokemonList = [
-        {
-            name: 'Pikachu', 
-            height: 1.3, 
-            type: 'electric',
-        },
-        {
-            name: 'Charizard',
-            height: 5.5,
-            type: ['fire', 'flying'],
-        },
-        {
-            name: 'Venusaur', 
-            height: 6.5,
-            type: 'electric',
-        },
-        {
-            name: 'Squirtle',
-            height: 1.6,
-            type: ['grass', 'poison']
-        }
-        ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=150&limit=150'
 
     function addPokemon(pokemon){
         // Check if pokemon is an object
@@ -30,14 +10,14 @@ let pokemonRepository = (function(){
         }
         // add pokemon object to list
         pokemonList.push(pokemon);
-   }
+    }
 
     function getAll(){
         // return list of pokemon objects
         return pokemonList;
-   }
+    }
 
-    function addListItem(pokemon) {
+    function addListItem(pokemon){
         let name = pokemon.name;
         let selectUl = document.getElementsByClassName('pokemon-list')[0];
         let li = document.createElement('li')
@@ -51,25 +31,97 @@ let pokemonRepository = (function(){
         li.appendChild(button);
         // add li element to ul list
         selectUl.appendChild(li);
-   }
+    }
+
+    function showLoadingGif(){
+        let loadingGif = "img/gifs/BeanEater200px.gif";
+        let img = document.createElement('img');
+        img.src = loadingGif;
+        img.className = 'loading-icon';
+        document.getElementById('body').appendChild(img);
+    }
+
+    function hideLoadingGif(){
+        let element = document.getElementsByClassName('loading-icon');
+        element[0].parentNode.removeChild(element[0]);
+    }
+
+    function loadList(){
+        showLoadingGif();
+        // Hit pokemon API
+        return fetch(apiUrl).then(function(response){
+            return response.json();
+        }).then(function(json){
+            // store pokemon name & details url in an array
+            json.results.forEach(function(item){
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                addPokemon(pokemon);
+            });
+            hideLoadingGif();
+        }).catch(function(e){
+            hideLoadingGif();
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item){
+        // set url to item url from loadList()
+        let url = item.detailsUrl;
+        // Hit pokemon API
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          // Add the details information to the item. This is utilized in showDetails()
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+    }
 
     function showDetails(pokemon){
+        console.log(pokemon)
         // show pokemon details on click event
-        let name = pokemon.name;
-        console.log(name);
-   }
+        loadDetails(pokemon).then(function(){
+        });
+    }
 
    return {
     addPokemon: addPokemon,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
    };
 })();
 
-// Test to add pokemon to pokemonList
-pokemonRepository.addPokemon({name: 'Wartortle', height: 3.03, type: 'water'})
+function sortListAZ(list) {
+    let sorted = list.sort(function(a,b){
+        let nameA = a.name
+        let nameB = b.name
+        if (nameA < nameB){
+            return -1;
+        }
+        else if (nameA > nameB){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    });
+    return sorted;
+};
 
-// loop through pokemonList and display as HTML
-pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function(){
+    let pokemonList = sortListAZ(pokemonRepository.getAll());
+    // loop through pokemonList and display as HTML
+    pokemonList.forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
 });
+
