5d41f556a1dcef2f58ef6d9e97b1007d7d40d58a
njneubauer <njneubauer@gmail.com>
2021-09-28 22:28:11 -0500

    forgot to add pokemon to showDetails(pokemon) to fix typo.

--- js/scripts.js
+++ js/scripts.js
@@ -85,7 +85,7 @@ let pokemonRepository = (function(){
         });
     }
 
-    function showDetails(){
+    function showDetails(pokemon){
         // show pokemon details on click event
         loadDetails(pokemon).then(function(){
             // assign variables to data
